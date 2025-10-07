1b2e30c213958aa757e25547b215bada727ab24d
Romanchuk Konstantin <@>
2025-10-07 16:11:41 +0600

    Create icons components

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+  icon: string
+  size?: number | string
+  color?: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <svg
+    :width="size || 20"
+    :height="size || 20"
+    :fill="color || '#000'"
+    aria-hidden="true"
+  >
+    <use :href="`#${props.icon}`" />
+  </svg>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import router from './router'
 
 const app = createApp(App)
 
+app.component("SvgIcon", SvgIcon)
+app.component("SvgHolder", SvgHolder)
 app.use(createPinia())
 app.use(router)
 
