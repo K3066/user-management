2be36cbab4d95cff89ea023ad0ab57a7b9fc2114
Romanchuk Konstantin <@>
2025-10-07 16:12:30 +0600

    Install naive UI

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import naive from 'naive-ui'
+import SvgIcon from '@/components/SvgIcon.vue'
+import SvgHolder from '@/components/SvgHolder.vue'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +12,7 @@ const app = createApp(App)
 app.component("SvgIcon", SvgIcon)
 app.component("SvgHolder", SvgHolder)
 app.use(createPinia())
+app.use(naive)
 app.use(router)
 
 app.mount('#app')
